575dcd8feec60eddd72722505f3b19a3fb6d5afd
josh <urgosxd@gmail.com>
2024-12-03 22:52:23 -0500

    aoe

--- src/app/api/ipn/route.ts
+++ src/app/api/ipn/route.ts
@@ -26,30 +26,30 @@ export async function POST(request: NextRequest) {
 };
 
 
-const paymentDataIPN = document
-  console.log("IPN:", paymentDataIPN);
-  /* Retrieve the IPN content */
-  const formAnswer = paymentDataIPN["kr-answer"];
-  const hash = paymentDataIPN["kr-hash"];
-  const hashKey = paymentDataIPN["kr-hash-key"];
+// const paymentDataIPN = document
+//   console.log("IPN:", paymentDataIPN);
+//   /* Retrieve the IPN content */
+//   const formAnswer = paymentDataIPN["kr-answer"];
+//   const hash = paymentDataIPN["kr-hash"];
+//   const hashKey = paymentDataIPN["kr-hash-key"];
 
 
 
-if(!checkHash(formAnswer, hash, hashKey) ) {
-    return NextResponse.json({
-      "message": "error"
-    },{status: 400})
-  }
+// if(!checkHash(formAnswer, hash, hashKey) ) {
+//     return NextResponse.json({
+//       "message": "error"
+//     },{status: 400})
+//   }
 
-/* Retrieve the transaction id from the IPN data */
-  const transaction = formAnswer.transactions[0];
+// /* Retrieve the transaction id from the IPN data */
+//   const transaction = formAnswer.transactions[0];
 
-  /* get some parameters from the answer */
-  const orderStatus = formAnswer.orderStatus;
-  const orderId = formAnswer.orderDetails.orderId;
-  const transactionUUID = transaction.uuid;
+//   /* get some parameters from the answer */
+//   const orderStatus = formAnswer.orderStatus;
+//   const orderId = formAnswer.orderDetails.orderId;
+//   const transactionUUID = transaction.uuid;
 
-  console.log(orderStatus)
+//   console.log(orderStatus)
   // const data = await createFormToken(document)
 
   // This will revalidate any URL that matches the provided page file on the next page visit.
